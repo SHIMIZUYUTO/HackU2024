696726245ead410973fe47cd7cdddb2604945ea0
SHIMIZUYUTO <s2221037@cco.kanagawa-it.ac.jp>
2024-08-18 15:24:41 +0900

    Step3

--- tegana/lib/main_Shimizu.dart
+++ tegana/lib/main_Shimizu.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp()); /*1*/
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  /*2*/
   @override
   Widget build(BuildContext context) {
-    /*3*/
   final wordPair = WordPair.random();
     return MaterialApp(
-      /*4*/
       title: 'Welcome to Flutter',
       home: Scaffold(
-        /*5*/
         appBar: AppBar(
-          /*6*/
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          /*7*/
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget{ 
+  @override
+  RandomWordState createState() => new RandomWordState();
 }

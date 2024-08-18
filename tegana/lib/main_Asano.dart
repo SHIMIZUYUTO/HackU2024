c0f229971fb56e8b9b0ba62c2fd3841afe693db2
haru1119 <s2221140@cco.kanagawa-it.ac.jp>
2024-08-18 12:33:11 +0900

    Step3

--- tegana/lib/main_Asano.dart
+++ tegana/lib/main_Asano.dart
@@ -4,25 +4,30 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp()); /*1*/
 
 class MyApp extends StatelessWidget {
-  /*2*/
-  @override
-  Widget build(BuildContext context) {
-    /*3*/
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      /*4*/
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        /*5*/
-        appBar: AppBar(
-          /*6*/
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          /*7*/
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
-    );
-  }
+   @override
+      Widget build(BuildContext context) {
+        final wordPair = WordPair.random();
+        return MaterialApp(
+          title: 'Welcome to Flutter',
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('Welcome to Flutter'),
+            ),
+            body: Center(
+              child: RandomWords(),
+            ),
+          ),
+        );
+      }
 }
+ class RandomWordsState extends State<RandomWords> {
+  @override
+      Widget build(BuildContext context) {
+        final wordPair = WordPair.random();
+        return Text(wordPair.asPascalCase);
+      }
+    }
+ class RandomWords extends StatefulWidget {
+      @override
+      RandomWordsState createState() => new RandomWordsState();
+    }

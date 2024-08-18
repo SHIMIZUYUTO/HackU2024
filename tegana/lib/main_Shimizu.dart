f37ca7cacf606fed7348acb50cd072142f2257de
SHIMIZUYUTO <s2221037@cco.kanagawa-it.ac.jp>
2024-08-18 10:46:14 +0900

    こんにちは
    こんにちは

--- tegana/lib/main_Shimizu.dart
+++ tegana/lib/main_Shimizu.dart
@@ -1,20 +1,20 @@
-    import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
     
-    void main() => runApp(MyApp()); /*1*/
+void main() => runApp(MyApp()); /*1*/
     
-    class MyApp extends StatelessWidget {/*2*/
-      @override
-      Widget build(BuildContext context) { /*3*/
-        return MaterialApp(  /*4*/
-          title: 'Welcome to Flutter',
-          home: Scaffold( /*5*/
-            appBar: AppBar( /*6*/
-              title: Text('Welcome to Flutter'),
-            ),
-            body: Center( /*7*/
-              child: Text('こんにちは'),
-            ),
-          ),
-        );
-      }
-    }
+class MyApp extends StatelessWidget {/*2*/
+  @override
+  Widget build(BuildContext context) { /*3*/
+    return MaterialApp(  /*4*/
+      title: 'Welcome to Flutter',
+      home: Scaffold( /*5*/
+        appBar: AppBar( /*6*/
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center( /*7*/
+          child: Text('こんにちは'),
+        ),
+      ),
+    );
+  }
+}

0b74fc8becd0b13ca0cb525a4b3794abdaffbd0b
s2221163 <s2221163@cco.kanagawa-it.ac.jp>
2024-08-18 13:36:34 +0900

    step4
    実行できないけど一応コミットしておく

--- tegana/lib/main_Kono.dart
+++ tegana/lib/main_Kono.dart
@@ -5,26 +5,49 @@
     class MyApp extends StatelessWidget {/*2*/
       @override
       Widget build(BuildContext context) { /*3*/
-        final wordPair = WordPair.random();
         return MaterialApp(  /*4*/
-          title: 'Welcome to Flutter',
-          home: Scaffold( /*5*/
-            appBar: AppBar( /*6*/
-              title: Text('Welcome to Flutter'),
-            ),
-            body: Center( /*7*/
-              child: RandomWords(),
-            ),
-          ),
+          title: 'Startup Name Generator',
+          home: RandomWords()
         );
       }
     }
 
     class RandomWordsState extends State<RandomWords>{
+      final _suggestions = <WordPair>[];
+      final _biggerFont = const TextStyle(fontSize: 18.0); 
+
       @override
       Widget build(BuildContext context){
-        final wordPair = WordPair.random();
-        return Text(wordPair.asPascalCase);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Startup Name Generator'),
+          ),
+         body: _buildSuggestions(), 
+        );
+      }
+
+       Widget _buildSuggestions(){
+        return ListView.builder(
+          padding:const EdgeInsets.all(16.0),
+          itemBuilder:/*1*/(context,i){
+            if(i.isOdd) return Divider();/*2*/
+
+            final index = i ~/ 2;/*3*/
+            if(index >= _suggestions.length){
+              _suggestions.addAll(generateWordPairs().take(10));/*4*/
+            }
+            return _buildRow(_suggestions[index]);/*5*/
+          }
+        );
+      }
+
+      Widget _buildRow(WordPair pair){
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ) ,
+        );
       }
     }
     

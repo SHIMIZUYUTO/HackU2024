e53061e80225dab12e04c76f49729b60e1784ccd
SHIMIZUYUTO <s2221037@cco.kanagawa-it.ac.jp>
2024-08-19 10:18:57 +0900

    シン・メイン

---
+++ tegana/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'メインです',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}

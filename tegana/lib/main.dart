13fb7a27341a0de681ac0339b45d8683ea2b68d4
SHIMIZUYUTO <s2221037@cco.kanagawa-it.ac.jp>
2024-08-21 11:53:28 +0900

    デモページ（途中経過2）

--- tegana/lib/main.dart
+++ tegana/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:typed_data'; // Uint8Listに必要
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,7 +42,7 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         '/home': (BuildContext context) => new MainPage(),
         '/testPhoto': (BuildContext context) => new Photo(),
-//        '/subpage2': (BuildContext context) => new test()
+        '/testYomifuda': (BuildContext context) => new Yomifuda()
       },
     );
   }
@@ -110,12 +111,6 @@ class _MyHomePageState extends State<MyHomePage> {
         _imageBytes = bytes;
       });
 
-      ElevatedButton(
-        onPressed: () =>
-          Navigator.of(context).pushNamed("/testPhoto"), //遷移先の指定
-          child: new Text('Photoページへ'),
-      );
-
     } else {
       print('No image selected.');
     }
@@ -128,13 +123,98 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: _imageBytes == null 
-            ? Text('No image selected.') 
-            : Image.memory(_imageBytes!),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_imageBytes == null)
+              Text('No image selected.')
+            else
+              Column(
+                children: [
+                  Image.memory(_imageBytes!),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // ここに画面遷移のロジックを追加
+                      Navigator.of(context).pushNamed('/Yomifuda');
+                    },
+                    child: Text('テキストページへ進む'),
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getImage,
         child: Icon(Icons.image),
+      ),
+    );
+  }
+}
+
+// ここから下は読み札登録の例ですよ。
+class Yomifuda extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('読み札登録の例'),
+      ),
+      body: TextInputWidget(),
+    );
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  TextEditingController _controller = TextEditingController();
+  FlutterTts flutterTts = FlutterTts();
+
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage("ja-JP"); // 日本語に設定
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'テキストを入力してください',
+            ),
+          ),
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text('入力されたテキスト: ${_controller.text}'),
+                  );
+                },
+              );
+              _speak(_controller.text); // テキストを読み上げ
+            },
+            child: Text('送信と読み上げ'),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: new Text('戻る'),
+          )
+        ],
       ),
     );
   }

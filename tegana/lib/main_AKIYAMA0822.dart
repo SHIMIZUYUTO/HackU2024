45e51cbccbbcaa3e4c65cfae35cde941cca000fd
KAITba2 <s2221111@cco.kanagawa-it.ac.jp>
2024-08-22 10:42:21 +0900

    くっつけたやつ

--- tegana/lib/main_AKIYAMA0822.dart
+++ tegana/lib/main_AKIYAMA0822.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:typed_data'; // Uint8Listに必要
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:english_words/english_words.dart';
+import 'package:tegana/main_test_tex.dart';
 
 void main() => runApp(MyApp());
 
@@ -248,4 +250,62 @@ class _TextInputWidgetState extends State<TextInputWidget> {
       ),
     );
   }
+}
+//こっからテキストボックス
+
+class inText extends StatelessWidget {
+  /*2*/
+  @override
+  Widget build(BuildContext context) {
+    /*3*/
+    return Scaffold(
+        /*4*/
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+        ),
+        body: RandomWords());
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]); /*5*/
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }

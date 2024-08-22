90ea107f0a0b5b1f59ccdde5498c4c85f64e08c8
SHIMIZUYUTO <s2221037@cco.kanagawa-it.ac.jp>
2024-08-22 10:29:51 +0900

    dartファイルの分割（テスト）

---
+++ tegana/lib/Shimizu_Yomifuda.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class Yomifuda extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('読み札登録の例'),
+      ),
+      body: TextInputWidget(),
+    );
+  }
+}
+
+class TextInputWidget extends StatefulWidget {
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  TextEditingController _controller = TextEditingController();
+  FlutterTts flutterTts = FlutterTts();
+
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage("ja-JP");
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'テキストを入力してください',
+            ),
+          ),
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text('入力されたテキスト: ${_controller.text}'),
+                  );
+                },
+              );
+              _speak(_controller.text);
+            },
+            child: Text('送信と読み上げ'),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('戻る'),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ tegana/lib/Shimizu_main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:typed_data'; // Uint8Listに必要
+import 'Shimizu_MainPage.dart';
+import 'Shimizu_Photo.dart';
+import 'Shimizu_Yomifuda.dart';
+
+void main() => runApp(MyApp());
+
+// Playerクラス
+class Player {
+  String name;
+  int score;
+
+  Player({
+    required this.name,
+    this.score = 0
+  });
+}
+
+// Cardクラス
+class Card {
+  String Y_Reading;
+  Uint8List? Y_Image;
+  Uint8List? E_Org;
+  Uint8List? E_Image;
+
+  Card({
+    required this.Y_Reading, 
+    required this.Y_Image, 
+    required this.E_Org, 
+    required this.E_Image,
+  });
+}
+
+// リストの宣言
+List<Player> playerList = [];
+List<Card> cardList = [];
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => MainPage(),
+        '/testPhoto': (BuildContext context) => Photo(),
+        '/testYomifuda': (BuildContext context) => Yomifuda()
+      },
+    );
+  }
+}

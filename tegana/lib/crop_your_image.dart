bfb3cbe566069f241aa74fcb2008532768c5b0ca
haru1119 <s2221140@cco.kanagawa-it.ac.jp>
2024-08-24 12:42:37 +0900

    crop1
    基本的なところ（できるかはわからん)

---
+++ tegana/lib/crop_your_image.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tegana/ImageEditPage.dart';
+import 'dart:typed_data'; // Uint8Listに必要
+import 'MainPage.dart';
+import 'Photo.dart';
+import 'Yomifuda.dart';
+import 'package:crop_your_image/crop_your_image.dart';
+import 'package:http/http.dart' as http;
+final _controller = CropController();
+
+Future<Uint8List> loadImageFromNetwork(String url) async{
+  final response = await http.get(Uri.parse(url));
+  if(response.statusCode == 200){
+    return response.bodyBytes;
+  } else {
+    throw Exception('Failed失敗したよごみが');
+  }
+  Uint8List imageBytes = await loadImageFromNetwork('https://img.gamewith.jp/img/6ed5f1964573e9f4a15b4ca90eb4e3fe.jpg');
+  @override
+  Widget build(BuildContext context) {
+    return Crop(
+      image:imageBytes,
+      controller: _controller,
+      onCropped: (image) {
+      // 切り抜き後の画像を使った処理
+    },
+  );
+}
+}

04c850867945830cbb64355b40f6932a4c2b1400
haru1119 <s2221140@cco.kanagawa-it.ac.jp>
2024-08-25 19:08:43 +0900

    右上丸

--- tegana/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tegana/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import file_selector_macos
 import flutter_image_compress_macos
 import flutter_tts
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }

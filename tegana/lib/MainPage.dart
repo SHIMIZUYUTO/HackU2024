7dc731e3e91c93423af0d19feb82262ed24e5f98
KAITba2 <s2221111@cco.kanagawa-it.ac.jp>
2024-08-25 10:42:20 +0900

    テスト用
    後で戻す

--- tegana/lib/MainPage.dart
+++ tegana/lib/MainPage.dart
@@ -18,40 +18,56 @@ class MainPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               ElevatedButton(
-                onPressed: () => Navigator.of(context).pushNamed("/testPhoto"),
-                child: const Text('遊ぶ',style: TextStyle(fontSize: 20,color: Colors.black87)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lightGreen[200],
-                  minimumSize: const Size(200, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                )
-              ),
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed("/testPhoto"),
+                  child: const Text('遊ぶ',
+                      style: TextStyle(fontSize: 20, color: Colors.black87)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightGreen[200],
+                    minimumSize: const Size(200, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  )),
               const SizedBox(height: 80),
               ElevatedButton(
-                onPressed: () => Navigator.of(context).pushNamed("/testPhoto"),
-                child: const Text('札を登録する',style: TextStyle(fontSize: 20,color: Colors.black87)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow[200],
-                  minimumSize: const Size(200, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                )      
-              ),
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed("/testPhoto"),
+                  child: const Text('札を登録する',
+                      style: TextStyle(fontSize: 20, color: Colors.black87)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow[200],
+                    minimumSize: const Size(200, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  )),
               const SizedBox(height: 80),
               ElevatedButton(
-                onPressed: () => Navigator.of(context).pushNamed("/testYomifuda"),
-                child: const Text('札を見る',style: TextStyle(fontSize: 20,color: Colors.black87)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.lightBlue[200],
-                  minimumSize: const Size(200, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                )
-              ),
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed("/testYomifuda"),
+                  child: const Text('札を見る',
+                      style: TextStyle(fontSize: 20, color: Colors.black87)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue[200],
+                    minimumSize: const Size(200, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  )),
+              const SizedBox(height: 80),
+              ElevatedButton(
+                  onPressed: () =>
+                      Navigator.of(context).pushNamed("/loasImage"),
+                  child: const Text('test',
+                      style: TextStyle(fontSize: 20, color: Colors.black87)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue[200],
+                    minimumSize: const Size(200, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  )),
             ],
           ),
         ),

532f6fbbc0fb6edba5624ffdfa7638c5df45e15f
SHIMIZUYUTO <s2221037@cco.kanagawa-it.ac.jp>
2024-08-27 19:30:07 +0900

    かるたっぽくして保存（重大なバグあり）
    いつの間にか、かるた印刷時のサイズが固定じゃなくなってた😢

--- tegana/lib/Photo.dart
+++ tegana/lib/Photo.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
 import 'dart:typed_data';
-import 'main.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:crop_your_image/crop_your_image.dart';
-
+import 'main.dart';
 
 class Photo extends StatelessWidget {
   const Photo({Key? key}) : super(key: key);
@@ -26,11 +27,28 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Uint8List? _imageBytes;
   Uint8List? _croppedImageBytes;
-  
   final picker = ImagePicker();
   final _cropController = CropController();
   final TextEditingController _textController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();  // フォームのキーを追加
+  final _formKey = GlobalKey<FormState>();
+  final GlobalKey _repaintBoundaryKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    _textController.addListener(_updateAvatar);
+  }
+
+  @override
+  void dispose() {
+    _textController.removeListener(_updateAvatar);
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _updateAvatar() {
+    setState(() {});
+  }
 
   Future<void> _getImage() async {
     try {
@@ -40,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
         final bytes = await pickedFile.readAsBytes();
         setState(() {
           _imageBytes = Uint8List.fromList(bytes);
-          _croppedImageBytes = null; // リセット
+          _croppedImageBytes = null;
         });
         _showCropDialog();
       } else {
@@ -65,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Crop(
                   image: _imageBytes!,
                   controller: _cropController,
-                  aspectRatio: 2/3,
+                  aspectRatio: 2 / 3,
                   onStatusChanged: (status) => print(status),
                   onCropped: (croppedData) {
                     if (!mounted) return;
@@ -96,27 +114,45 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _registerCard() {
+  Future<Uint8List?> _captureImage() async {
+    try {
+      RenderRepaintBoundary boundary = _repaintBoundaryKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+      return byteData?.buffer.asUint8List();
+    } catch (e) {
+      print('Error capturing image: $e');
+      return null;
+    }
+  }
+
+  void _registerCard() async {
     if (_formKey.currentState!.validate()) {
       if (_croppedImageBytes != null && _textController.text.isNotEmpty) {
-        final newCard = Cards(
-          Y_Reading: _textController.text,
-          Y_Image: null,  // 今回は使用しない
-          E_Org: _croppedImageBytes,
-          E_Image: null,  // 今回は使用しない
-        );
-        setState(() {
-          cardList.add(newCard);
-        });
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('カードが登録されました')),
-        );
-        // 入力をクリア
-        _textController.clear();
-        setState(() {
-          _croppedImageBytes = null;
-          _imageBytes = null;
-        });
+        Uint8List? capturedImage = await _captureImage();
+        if (capturedImage != null) {
+          final newCard = Cards(
+            Y_Reading: _textController.text,
+            Y_Image: null,
+            E_Org: capturedImage, // 頭文字や枠線を含めてキャプチャしたものを保存
+            E_Image: null,
+          );
+          setState(() {
+            cardList.add(newCard);
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('カードが登録されました')),
+          );
+          _textController.clear();
+          setState(() {
+            _croppedImageBytes = null;
+            _imageBytes = null;
+          });
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('画像のキャプチャに失敗しました')),
+          );
+        }
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('画像と読み札の両方を入力してください')),
@@ -127,6 +163,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    String firstChar = _textController.text.isNotEmpty ? _textController.text[0] : '';
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -164,62 +202,83 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Expanded(
               child: GestureDetector(
-                child: InkWell(
-                borderRadius: BorderRadius.circular(8.0),
                 onTap: _getImage,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 10),
-                    color: Colors.red,
-                  ),
-                    margin: const EdgeInsets.all(20),
-                    child: _croppedImageBytes != null
-                      ? Image.memory(_croppedImageBytes!)
-                      : _imageBytes != null
-                          ? Image.memory(_imageBytes!)
-                          : const Center(
-                              child: Text(
-                                '画像を選択',
-                                style: TextStyle(color: Colors.white, fontSize: 50),
-                              ),
+                child: RepaintBoundary(
+                  key: _repaintBoundaryKey,
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 10),
+                          color: Colors.red,
+                        ),
+                        child: _croppedImageBytes != null
+                            ? Image.memory(_croppedImageBytes!)
+                            : _imageBytes != null
+                                ? Image.memory(_imageBytes!)
+                                : const Center(
+                                    child: Text(
+                                      '画像を選択',
+                                      style: TextStyle(color: Colors.white, fontSize: 50),
+                                    ),
+                                  ),
+                      ),
+                      Positioned(
+                        top: 30,
+                        right: 30,
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Colors.green,
+                          child: Text(
+                            firstChar,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
                             ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            ),
+
+            // 読み札入力部分
             Container(
               color: Colors.green,
               padding: const EdgeInsets.all(20),
-              child: Form(  // Formウィジェットでラップ
-                key: _formKey,  // フォームのキーを指定
-                  child: Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          print('読み札ボタンが押されました');
-                        },
-                        child: const Text('読札'),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        print('読み札ボタンが押されました');
+                      },
+                      child: const Text('読み札'),
+                    ),
+                    TextFormField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: 'ここに入力',
                       ),
-                      TextFormField(  // TextFieldをTextFormFieldに変更
-                          controller: _textController,
-                          decoration: InputDecoration(
-                            hintText: 'ここに入力',
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {    // 入力欄が空欄だった場合
-                              return 'テキストが入力されていません'; // エラーメッセージ
-                            }
-                            if(!RegExp(r'^[ぁ-ゖァ-ヶー]*$').hasMatch(value)){     // ひらがな、カタカナ以外が入力されている場合
-                              return 'ひらがなまたはカタカナを入力してください';  // エラーメッセージ 
-                            }
-                            return null; // エラーがない場合はnullを返す
-                          },
-                        ),
-                    ],
-                  ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'テキストが入力されていません';
+                        }
+                        if (!RegExp(r'^[ぁ-ゖァ-ヶー]*$').hasMatch(value)) {
+                          return 'ひらがなまたはカタカナを入力してください';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-
           ],
         ),
       ),
